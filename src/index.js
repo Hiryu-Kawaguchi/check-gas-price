a1543848d060ed07127dd119296e1298822a5984
Glitch (check-gas-price) <none>
2018-11-11 15:06:24 +0000

    🍔🏰 Updated with Glitch

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import Bulma
+require('./mystyles.scss');
+// const Web3 = require('web3');
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io'));
+// web3.eth.getGasPrice((error, result) => {
+//   console.log(result.toString(10));
+//   const gasPriceInGwei = web3.fromWei(result, 'gwei');
+//   console.log(gasPriceInGwei);
+// });
+
